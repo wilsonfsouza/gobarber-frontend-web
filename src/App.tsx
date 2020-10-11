abae11f43b382b72579d202df903ddd3f48fad3a
Wilson Franca <wilson.franca.92@gmail.com>
2020-10-11 12:30:13 -0700

    removed React files not related to this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 
-function App() {
+const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
   );
-}
+};
 
 export default App;
