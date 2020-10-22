b27e5dd92b8984bfcb8b246eab7c07838e7c27aa
Wilson Franca <wilson.franca.92@gmail.com>
2020-10-21 18:37:07 -0700

    wrapped the authentication context around the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FunctionComponent = () => {
   return (
     <>
-      <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
       <GlobalStyle />
     </>
   );
