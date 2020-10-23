d2b8e0d8f1ff1204740a83f1b7c2688fc38122d7
Wilson Franca <wilson.franca.92@gmail.com>
2020-10-22 20:15:14 -0700

    replaced contexts with AppProvider components that holds all my contexts

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-import ToastContainer from './components/ToastContainer';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FunctionComponent = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
-      <ToastContainer />
+      </AppProvider>
       <GlobalStyle />
     </>
   );
