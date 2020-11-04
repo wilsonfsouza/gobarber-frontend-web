7564f4df347e2f02ed0e240b256df211cab794db
Wilson Franca <wilson.franca.92@gmail.com>
2020-11-03 19:23:03 -0800

    removed each route declaration to use the router created

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
-
 import AppProvider from './hooks';
+import Routes from './routes';
 
 const App: React.FunctionComponent = () => {
   return (
-    <>
+    <BrowserRouter>
       <AppProvider>
-        <SignIn />
+        <Routes />
       </AppProvider>
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 };
 
