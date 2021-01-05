8936c80efd96daba494ab015942fe6eab2b3f344
Wilson Franca <wilson.franca.92@gmail.com>
2021-01-04 18:17:27 -0800

    feat(password recovery): added reset password page, accessible by the link generated by forgot password.

---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,107 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { useHistory, useLocation } from 'react-router-dom';
+import getValidationErrors from '../../utils/getValidationErrors';
+import LogoImg from '../../assets/images/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Content, AnimationContainer, Background } from './styles';
+import { useToast } from '../../hooks/toast';
+import api from '../../services/api';
+
+interface ResetPasswordFormData {
+  password: string;
+  password_confirmation: string;
+}
+const ResetPassword: React.FunctionComponent = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const location = useLocation();
+
+  const handleFormSubmit = useCallback(
+    async (data: ResetPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          password: Yup.string().required('Required password'),
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref('password'), undefined],
+            'Passwords must match',
+          ),
+        });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const { password, password_confirmation } = data;
+
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Error on reseting password',
+          description:
+            'An error has occurred when attempting to reset your password, please try again.',
+        });
+      }
+    },
+    [addToast, history, location.search],
+  );
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={LogoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleFormSubmit}>
+            <h1>Reset password</h1>
+
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="New Password"
+            />
+
+            <Input
+              name="password_confirmation"
+              icon={FiLock}
+              type="password"
+              placeholder="Password Confirmation"
+            />
+
+            <Button type="submit">Reset password</Button>
+          </Form>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ResetPassword;
---
+++ src/pages/ResetPassword/styles.ts
@@ -0,0 +1,80 @@
+import styled, { keyframes } from 'styled-components';
+import signInBackgroundImg from '../../assets/images/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px)
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0)
+  }
+`;
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
+  form {
+    margin: 5rem;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 1.5rem;
+    }
+
+    a {
+      color: var(--color-white);
+      text-decoration: none;
+      display: block;
+      margin-top: 1.5rem;
+      transition: color 0.2s;
+
+      &:hover {
+        color: var(--color-white-darken);
+      }
+    }
+  }
+  > a {
+    color: var(--color-primary);
+    text-decoration: none;
+    display: block;
+    margin-top: 1.5rem;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: var(--color-primary-darken);
+    }
+    svg {
+      margin-right: 1rem;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
