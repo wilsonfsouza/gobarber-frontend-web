4e3f479b6c0590057c94d9fdd9d151b27c1b11b9
Wilson Franca <wilson.franca.92@gmail.com>
2021-01-09 14:55:01 -0800

    feat(routes): added private route to user profile.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+
 import Route from './Route';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FunctionComponent = () => (
   <Switch>
@@ -15,6 +19,7 @@ const Routes: React.FunctionComponent = () => (
     <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/profile" component={Profile} isPrivate />
   </Switch>
 );
 
