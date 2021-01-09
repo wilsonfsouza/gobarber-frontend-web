22ee06488de020269b56f8c582fbda9f5fa94dd5
Wilson Franca <wilson.franca.92@gmail.com>
2021-01-09 14:54:17 -0800

    feat(input component): added container style property to pass a style to the container around the input component.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -12,11 +12,13 @@ import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
+  containerStyle?: object;
   icon?: React.ComponentType<IconBaseProps>;
 }
 
 const Input: React.FunctionComponent<InputProps> = ({
   name,
+  containerStyle = {},
   icon: Icon,
   ...rest
 }) => {
@@ -43,7 +45,12 @@ const Input: React.FunctionComponent<InputProps> = ({
   }, [fieldName, registerField]);
 
   return (
-    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+    <Container
+      style={containerStyle}
+      isErrored={!!error}
+      isFilled={isFilled}
+      isFocused={isFocused}
+    >
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
