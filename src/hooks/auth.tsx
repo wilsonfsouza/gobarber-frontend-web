9b52655b0067768c42775ac69ff78fdbabcd0ac8
Wilson Franca <wilson.franca.92@gmail.com>
2021-01-09 14:52:52 -0800

    feat(authorization hook): added function to update user information and set updated information to local storage.

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,19 +1,27 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string | undefined;
+  name: string;
+  email: string;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
 }
 interface AuthContextProps {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
 interface AuthDataProps {
   token: string;
-  user: object;
+  user: User;
 }
 
 const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
@@ -23,6 +31,7 @@ const AuthProvider: React.FC = ({ children }) => {
     const token = localStorage.getItem('@GoBarber:token');
     const user = localStorage.getItem('@GoBarber:user');
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
 
@@ -39,6 +48,8 @@ const AuthProvider: React.FC = ({ children }) => {
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
     setAuthData({ token, user });
   }, []);
 
@@ -49,8 +60,22 @@ const AuthProvider: React.FC = ({ children }) => {
     setAuthData({} as AuthDataProps);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setAuthData({
+        token: authData.token,
+        user,
+      });
+    },
+    [setAuthData, authData.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: authData.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: authData.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
